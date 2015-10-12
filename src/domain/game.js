f1a6db512826c020e56c8ce9354bf1bcc7ed452d
Tom Martin <tom@tpgmartin.com>
2015-10-12 11:58:31 +0000

    Add tests for moving in all directions, refactoring

--- src/domain/game.js
+++ src/domain/game.js
@@ -9,6 +9,18 @@ function Game(arena) {
 //moves - [L,F,R,F,F] - set of actions to be taken
 //returns updated robot position
 Game.prototype.moveRobot = function moveRobot(robot, moves) {
+  var direction = robot.direction;
+  if (moves.length > 0) {
+    if (direction === 'N') {
+      robot.y += 1; 
+    } else if (direction === 'S') {
+      robot.y -= 1; 
+    } else if (direction === 'E') {
+      robot.x += 1; 
+    } else if (direction === 'W') {
+      robot.x -= 1; 
+    }
+  }
   return robot;
 };
 
--- tests/gameTests.js
+++ tests/gameTests.js
@@ -2,17 +2,25 @@
 
 var game = require("../src/domain/game.js");
 var assert = require("assert");
+var data_driven = require('data-driven');
 
 describe('moveRobot', function() {
-  it('should return robot', function () {
-    var subject = new game();
-    var robot = {x: 10, y: 10, direction: 'N'};
-    var moves = [];
+  describe('when executing set of moves', function() {
+    data_driven([{moves: [], direction: 'W', expectedX: 10, expectedY:10},
+                 {moves: ['M'], direction: 'W', expectedX: 9, expectedY:10},
+                 {moves: ['M'], direction: 'E', expectedX: 11, expectedY:10},
+                 {moves: ['M'], direction: 'S', expectedX: 10, expectedY: 9},
+                 {moves: ['M'], direction: 'N', expectedX: 10, expectedY: 11}] , function() {
+      it('should move {direction} facing robot for {moves} on ({expectedX}, {expectedY})', function (ctx) {
+        var subject = new game();
+        var robot = {x: 10, y: 10, direction: ctx.direction};
+        
+        var outcome = subject.moveRobot(robot, ctx.moves);
     
-    var outcome = subject.moveRobot(robot, moves);
-    
-    assert.equal(outcome.x, robot.x);
-    assert.equal(outcome.y, robot.y);
-    assert.equal(outcome.direction, robot.direction);
+        assert.equal(outcome.x, ctx.expectedX);
+        assert.equal(outcome.y, ctx.expectedY);
+        assert.equal(outcome.direction, robot.direction);  
+      });
+    });
   });
 });
